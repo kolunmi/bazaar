6d46e00cb33ad6c202baf89a7ef0c262ad45e42e
Tulip Blossom <tulilirockz@proton.me>
2025-07-16 00:01:34 -0300

    flatpak: simplify scripts and make them less verbose
    Signed-off-by: Tulip Blossom <tulilirockz@proton.me>

--- build-aux/flatpak/fusermount-wrapper.sh
+++ build-aux/flatpak/fusermount-wrapper.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
+set -eo pipefail
 echo "Running fusermount wrapper, redirecting to host..."
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/flatpak/bus
 
-if flatpak-spawn --host fusermount --version ; then
-  echo "Using fusermount."
-  binary="fusermount"
-else
-  # Some distros don't ship `fusermount` anymore, but `fusermount3` like Alpine
-  echo "Unable to execute fusermount, trying to use fusermount3."
+binary="fusermount"
+if flatpak-spawn --host fusermount3 --version &> /dev/null ; then
   binary="fusermount3"
 fi
 
 [ ! -z "$_FUSE_COMMFD" ] && export FD_ARGS="--env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD}"
-exec flatpak-spawn --host $FD_ARGS $binary "$@"
+exec flatpak-spawn --host ${FD_ARGS} "$binary" "$@"
