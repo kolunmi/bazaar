2f5048f7b4892df809edceb1b6112b2817d9c9e6
Eva M <kolunmi@posteo.net>
2026-02-13 14:24:37 -0700

    completely rewrite BzCarousel

--- src/bz-carousel.h
+++ src/bz-carousel.h
@@ -1,7 +1,7 @@
 /* bz-carousel.h
  *
- * Copyright 2025 Alexander Vanhee
- * Copyright (C) 2019 Alice Mikhaylenko <alicem@gnome.org>
+ * Copyright 2026 Eva M
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -20,29 +20,65 @@
 
 #pragma once
 
-#include <adwaita.h>
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
 #define BZ_TYPE_CAROUSEL (bz_carousel_get_type ())
-
 G_DECLARE_FINAL_TYPE (BzCarousel, bz_carousel, BZ, CAROUSEL, GtkWidget)
 
-GtkWidget *bz_carousel_new (void) G_GNUC_WARN_UNUSED_RESULT;
+GtkWidget *
+bz_carousel_new (void);
 
-void bz_carousel_set_widgets (BzCarousel *self,
-                              GList      *widgets);
+gboolean
+bz_carousel_get_auto_scroll (BzCarousel *self);
 
-void bz_carousel_scroll_to (BzCarousel *self,
-                            GtkWidget  *widget,
-                            gboolean    animate);
+gboolean
+bz_carousel_get_allow_long_swipes (BzCarousel *self);
 
-GtkWidget *bz_carousel_get_nth_page (BzCarousel *self,
-                                     guint       n);
+gboolean
+bz_carousel_get_allow_mouse_drag (BzCarousel *self);
 
-guint bz_carousel_get_n_pages (BzCarousel *self);
+gboolean
+bz_carousel_get_allow_scroll_wheel (BzCarousel *self);
 
-double bz_carousel_get_position (BzCarousel *self);
+gboolean
+bz_carousel_get_allow_raise (BzCarousel *self);
+
+gboolean
+bz_carousel_get_raised (BzCarousel *self);
+
+GtkSingleSelection *
+bz_carousel_get_model (BzCarousel *self);
+
+void
+bz_carousel_set_auto_scroll (BzCarousel *self,
+                             gboolean    auto_scroll);
+
+void
+bz_carousel_set_allow_long_swipes (BzCarousel *self,
+                                   gboolean    allow_long_swipes);
+
+void
+bz_carousel_set_allow_mouse_drag (BzCarousel *self,
+                                  gboolean    allow_mouse_drag);
+
+void
+bz_carousel_set_allow_scroll_wheel (BzCarousel *self,
+                                    gboolean    allow_scroll_wheel);
+
+void
+bz_carousel_set_allow_raise (BzCarousel *self,
+                             gboolean    allow_raise);
+
+void
+bz_carousel_set_raised (BzCarousel *self,
+                        gboolean    raised);
+
+void
+bz_carousel_set_model (BzCarousel         *self,
+                       GtkSingleSelection *model);
 
 G_END_DECLS
+
+/* End of bz-carousel.h */
