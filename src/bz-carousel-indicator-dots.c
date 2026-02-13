b875d13266e15467d862c2966353860b219027dd
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2026-02-13 19:44:17 +0100

    license of file doesn't match with its SPDX-License-Identifier
    Closes #1103

--- src/bz-carousel-indicator-dots.c
+++ src/bz-carousel-indicator-dots.c
@@ -15,7 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #include "bz-carousel-indicator-dots.h"
