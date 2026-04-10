a1ccf9b1e0fba5be58171f76ef0afae6dcb3973f
Eva M <kolunmi@posteo.net>
2026-04-09 22:54:23 -0700

    add horizontal fadeout mask to rid of sharp end of vertical gradient

--- src/bz-data-graph.c
+++ src/bz-data-graph.c
@@ -306,6 +306,35 @@ bz_data_graph_snapshot (GtkWidget   *widget,
          },
          },
       };
+      const GskColorStop hfadeout_mask_stops[] = {
+        {
+         0.0,
+         {
+         .red   = 0.0,
+         .green = 0.0,
+         .blue  = 0.0,
+         .alpha = 1.0,
+         },
+         },
+        {
+         0.75,
+         {
+         .red   = 0.0,
+         .green = 0.0,
+         .blue  = 0.0,
+         .alpha = 1.0,
+         },
+         },
+        {
+         1.0,
+         {
+         .red   = 0.0,
+         .green = 0.0,
+         .blue  = 0.0,
+         .alpha = 0.0,
+         },
+         },
+      };
 
       gtk_snapshot_append_stroke (
           snapshot,
@@ -359,18 +388,36 @@ bz_data_graph_snapshot (GtkWidget   *widget,
           snapshot,
           path,
           GSK_FILL_RULE_WINDING);
+
+      gtk_snapshot_push_mask (snapshot, GSK_MASK_MODE_ALPHA);
+
       gtk_snapshot_append_linear_gradient (
           snapshot,
           &GRAPHENE_RECT_INIT (
               0.0,
               0.0,
-              widget_width - LABEL_MARGIN,
+              end_position.x,
+              widget_height - LABEL_MARGIN),
+          &GRAPHENE_POINT_INIT (0.0, 0.0),
+          &GRAPHENE_POINT_INIT (end_position.x, 0.0),
+          hfadeout_mask_stops,
+          G_N_ELEMENTS (hfadeout_mask_stops));
+      gtk_snapshot_pop (snapshot); /* Push Mask (mask) */
+
+      gtk_snapshot_append_linear_gradient (
+          snapshot,
+          &GRAPHENE_RECT_INIT (
+              0.0,
+              0.0,
+              end_position.x,
               widget_height - LABEL_MARGIN),
           &GRAPHENE_POINT_INIT (0.0, 0.0),
           &GRAPHENE_POINT_INIT (0.0, widget_height - LABEL_MARGIN),
           gradient_stops,
           G_N_ELEMENTS (gradient_stops));
-      gtk_snapshot_pop (snapshot);
+
+      gtk_snapshot_pop (snapshot); /* Push Mask (image) */
+      gtk_snapshot_pop (snapshot); /* Push Fill */
     }
   gtk_snapshot_restore (snapshot);
 
